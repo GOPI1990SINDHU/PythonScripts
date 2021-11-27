85cb34d5a6d8756e87c3373db622c543a67e42fa
GOPI1990SINDHU <87268027+GOPI1990SINDHU@users.noreply.github.com>
2021-11-27 11:05:28 +0100

    Create FirstBadversionsearch_usingBinarysearch_algorithm.py

---
+++ FirstBadversionsearch_usingBinarysearch_algorithm.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+        While start < end:
+            mid = (start + end) // 2
+            if (isBadVersion(mid)):
+                end = mid
+            else:
+                start = mid + 1
+        return start
+        
